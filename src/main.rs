8143a7f47cfdf541eef99a844ff6d9849f2a8efa
ReetamBG <r33t4m@gmail.com>
2025-10-02 21:50:56 +0530

    Added writer for vga text buffer

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@
 
 use core::panic::PanicInfo;
 
-// override entry point
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
 // define our own panic handler
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+// override entry point
+// linker looks for a function named _start by default
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
+
+mod vga_buffer;
