06a090fbf20950fd71b1f6aad72907a267acc0a5
ReetamBG <r33t4m@gmail.com>
2025-10-23 17:36:13 +0530

    post 3 done

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,19 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // implementing our own panic hanlder
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 // linker looks for a function named _start as the entry point
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello world");
 
     loop {}
 }
